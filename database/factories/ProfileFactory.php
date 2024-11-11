b047f4d9cd383f97b19e4d7ba35183c116fcb0fa
pintsizedsalt <samlisingisaac28@gmail.com>
2024-11-11 21:02:33 +0800

    loh

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
+ */
+class ProfileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fake()->randomElement([1,2,3,4,5,6,7,8,9,10]),
+            'bio' => Str::random(5),
+            'school' => Str::random(10),
+        ];
+    }
+}
--- database/migrations/2024_11_11_114200_create_profiles_table.php
+++ database/migrations/2024_11_11_114200_create_profiles_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
             $table->string('bio');
             $table->string('school');
             $table->timestamps();
